bf20c2df450d91352850f5b2817e01757bd99742
gianniskokkosis <t8160190@aueb.gr>
2020-11-01 23:54:22 +0200

    Add some changes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,111 @@
+/* var firstName = 'John';
+var lastName = 'Kokkosis';
+var age = 22;
+
+var fullAge = true;
+var job = 'Product Manager';
+console.log(fullAge);
+console.log(job);
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old!'); */
+
+/**
+ * Variable and type coercion
+ */
+
+/*  var firstName = 'John';
+ var age = 22;
+ console.log(firstName + ' is ' + age + ' years old.');
+
+ var job = 'Product Manager';
+ var isMaried = false;
+
+ console.log(firstName + ' is ' + age + ' years old' + job + '. Is he married? ' + isMaried);
+ age = 'twenty two';
+ console.log(age);
+ job = 'Developer';
+ alert(job); */
+
+/* var wieghtMark, weightJohn, heightMark, heightJohn, markBMI, johnBMI
+
+wieghtMark = 80;
+weightJohn = 90;
+heightMark = 1.75;
+heightJohn = 1.80;
+
+markBMI = wieghtMark / (heightMark * heightMark);
+johnBMI = weightJohn / (heightJohn * heightJohn);
+
+isMarkHigher = markBMI > johnBMI;
+
+console.log('Is Mark\'s BMI higher than John\'s? ' + isMarkHigher);
+
+console.log(markBMI, johnBMI); */
+
+/* var civilStatus = '';
+
+if (civilStatus === 'maried') {
+    console.log(firstName + ' is maried');
+} else if (civilStatus === 'single') {
+    console.log(firstName + ' is single');
+} else if (civilStatus === 'divorced') {
+    console.log(firstName + ' is divorcerd');
+} else {
+    console.log(firstName + ' is maried and has kids!');
+} */
+
+/* var firstName = 'John';
+var age = 9;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man');
+} else {
+    console.log(firstName + ' is a man!');
+} */
+
+/* var firstName = 'John';
+var age = 90;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' dirnks juice');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+var isMaried = false;
+isMaried ? console.log('Maried') : console.log('Not maried');
+
+var maried = true;
+var isMaried = maried ? true : false;
+console.log(isMaried);
+
+var job = 'teacher';
+
+switch (job) {
+    case 'teacher':
+        console.log( firstName + ' is a ' + job);
+        break;
+    case 'Product Manager':
+        console.log(firstName + ' is a' + job);
+        break;
+    default:
+        console.log(firstName + ' is unemployed.');
+        break;
+} */
+
+var sumTeamJohn = (89 + 120 + 103);
+var sumTeamMike = (116 + 94 + 123);
+
+
+
+
+
+
+
+
+
+
+
+
